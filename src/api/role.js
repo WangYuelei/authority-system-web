7e54f32131ad0900ef3698724f4f186bdaa5b31e
84227396@qq.com <84227396@qq.com>
2022-06-28 18:17:50 +0800

    分配权限

--- src/api/role.js
+++ src/api/role.js
@@ -6,5 +6,33 @@ export default {
    */
   async getRoleListApi(params) {
     return await http.get("/api/role/list", params);
+  },
+  /**
+   * 添加角色
+   * @returns
+   */
+  async addRoleApi(params) {
+    return await http.post("/api/role/add", params);
+  },
+  /**
+   * 编辑角色
+   * @returns
+   */
+  async updateRoleApi(params) {
+    return await http.put("/api/role/update", params);
+  },
+  /**
+   * 查询分配权限树列表
+   * @returns
+   */
+  async getAssignTreeApi(params){
+    return await http.get("/api/role/getAssignPermissionTree",params);
+  },
+  /**
+   * 分配权限
+   * @returns
+   */
+  async assignSaveApi(params){
+    return await http.post("/api/role/saveRoleAssign",params);
   }
 }
